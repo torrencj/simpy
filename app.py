5a0899e206d757acaa2a76b3645828681efe444b
John <16932417+torrencj@users.noreply.github.com>
2017-10-20 14:09:49 -0500

    testing main routes

--- app.py
+++ app.py
@@ -16,6 +16,18 @@ app = Flask(__name__)
 #   browser.get_screenshot_as_base64('screenie.txt')
 #   return "Saved"
 
+@app.route('/')
+def hello():
+  display = Display(visible=0, size=(1024,800))
+  display.start()
+
+  print "Starting display..."
+  browser = webdriver.Firefox()
+  print "Loading site...."
+  browser.get('https://' + siteURL)
+  browser.get_screenshot_as_base64('screenie.txt')
+  return "Saved"
+
 @app.route('/<siteURL>')
 def get_pic(siteURL):
   display = Display(visible=0, size=(1024,800))
